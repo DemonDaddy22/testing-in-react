4c66199ad1ca9d2ae87b83831e9c04ae015c91a6
Rohan Gupta <39908472+DemonDaddy22@users.noreply.github.com>
2025-09-30 12:00:40 +0530

    Add RTL and Jest dependencies

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  moduleNameMapper: {
+    // Handle CSS imports (if you use CSS modules or global CSS)
+    '^.+\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+
+    // Handle path aliases from tsconfig.json
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
+
+export default config;
